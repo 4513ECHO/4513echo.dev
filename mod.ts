1236d9631a55347dae4c7a5fa55a7b3e76fda206
4513ECHO <4513echo@gmail.com>
2022-07-19 17:12:54 +0900

    feat: Add alias for my dotfiles

--- mod.ts
+++ mod.ts
@@ -1,5 +1,22 @@
-import { serve } from "https://deno.land/x/sift@0.1.6/mod.ts";
+import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
+import {
+  Status,
+  STATUS_TEXT,
+} from "https://deno.land/std@0.148.0/http/http_status.ts";
 
 serve({
   "/": () => new Response("Hello, World!"),
+  "/dot": () => {
+    const [status, statusText] = [
+      Status.MovedPermanently,
+      STATUS_TEXT[Status.MovedPermanently],
+    ];
+    const location =
+      "https://raw.githubusercontent.com/4513ECHO/dotfiles/main/up";
+    return new Response(`${status}: ${statusText}`, {
+      status,
+      statusText,
+      headers: { location },
+    });
+  },
 });
